39776911814b27fa7e8d32086e71dd493f7da824
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-14 17:52:32 -0700

    added Comment model

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+     id:{
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+     },
+     text: {
+        type: DataTypes.STRING,
+        allowNull: false
+     },
+     postId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model:'post',
+            key: 'id'
+        }
+     },
+     userId: {
+        type:DataTypes.INTEGER,
+        references: {
+            model:'user',
+            key:'id'
+        }
+     }
+    }, {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+)
+
+module.exports = Comment;
