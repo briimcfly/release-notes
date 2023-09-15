4fc405887f5b86340eaecdb6c7442a7dc4e01b01
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-15 12:53:26 -0700

    displaying post data on cards

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {Post, Comment, User} = require('../models')
+
+router.get('/', async(req,res)=>{
+    try{
+        const postData = await Post.findAll({
+            // include: [{model: User}, {model: Comment}]
+        });
+        
+        const posts = postData.map((post) => post.get({plain:true}))
+
+        res.render('homepage', {
+            posts
+        });
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
