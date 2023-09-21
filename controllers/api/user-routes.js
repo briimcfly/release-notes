c96ebe461971981336619d45e8eb2249dbe5b85e
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-21 10:05:31 -0700

    modified login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,40 +13,54 @@ router.post('/', async (req,res) => {
             res.status(200).json(dbUserData);
     } catch (err) {
         console.error("There was a problem creating a new user", err)
-        res.status(500).json(err);
+        res.status(500).json({message: "Internal Server Error"});
     }
 });
 
 // Let a user Login 
 router.post('/login', async(req, res) => {
+    //User Username
+    let userData;
     try{
-        const userData = await User.findOne({
+        userData = await User.findOne({
             where: {
                 username: req.body.username
             }
         });
-        
-        //No User found
-        if (!userData) {
-            res.status(400).json({message: 'Incorrect Username'});
-        }
-
-        const userPass = await userData.checkPassword(req.body.password);
-
-        if (!userPass) {
-            res.status(400).json({message: 'Incorrect Password'});
-        }
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            req.session.userId = userData.isSoftDeleted;
-            res.status(200).json({message: "User successfully logged in"});
-        })
-
     }catch (err) {
-        console.error("There was a problem logging in", err)
-        res.status(500).json(err)
+        console.error("There was an issue finding Username", err)
+        return res.status(500).json({message: "Internal Server Error"})
     }
-})
+
+    //Bad Username 
+    if (!userData) {
+        return res.status(400).json({message: 'Incorrect Username or Password'});
+    }
+
+    //User Password 
+    let userPass;
+
+    try{
+        userPass = await userData.checkPassword(req.body.password);
+    } catch(err){
+        console.error("There was an issue checking password", err)
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+
+    //Bad password
+    if (!userPass) {
+        return res.status(400).json({message: 'Incorrect Username or Password'});
+    }
+
+    //Save Session
+    req.session.save(err => {
+        if (err) {
+            console.error("There was an issue saving session", err)
+            return res.status(500).json({message: "Internal Server Error"});
+        }
+        req.session.loggedIn = true;
+        res.status(200).json({message: "User successfully logged in"})
+    }); 
+});
 
 module.exports = router;
